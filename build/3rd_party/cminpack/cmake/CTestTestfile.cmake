288f6b92dd8677efdb2e8d4eba929584a769f4f4
simondeu <simondeuten@gmail.com>
2026-03-11 17:49:20 +0100

    Setup

---
+++ build/3rd_party/cminpack/cmake/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: D:/GEO/GEO1016/Assignment2/A2_Triangulation_Code/3rd_party/cminpack/cmake
+# Build directory: D:/GEO/GEO1016/Assignment2/A2_Triangulation_Code/build/3rd_party/cminpack/cmake
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
