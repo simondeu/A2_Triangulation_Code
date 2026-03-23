cbc966013994c893078fc500e25e43ad175e3eca
Daman Dogra <damandograa@gmail.com>
2026-03-23 08:54:24 +0100

    Stop tracking build and IDE files

--- build/3rd_party/cminpack/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/GEO/GEO1016/Assignment2/A2_Triangulation_Code/3rd_party/cminpack
-# Build directory: D:/GEO/GEO1016/Assignment2/A2_Triangulation_Code/build/3rd_party/cminpack
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("cmake")
